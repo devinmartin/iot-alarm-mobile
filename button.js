e0d0c399734345b82c186012f9258203732bb4ab
devinmartin <devin@devin-martin.com>
2018-09-22 16:11:13 -0600

    Add a timer to the button and led feedback

--- button.js
+++ button.js
@@ -5,18 +5,31 @@ load('api_mqtt.js');
 load('api_net.js');
 load('api_sys.js');
 load('api_timer.js');
-load('api_shadow.js');
 load('api_azure.js');
 
-let led = Cfg.get('pins.led');  // Built-in LED GPIO number
+let led = Cfg.get('pins.led');
 let button = Cfg.get('pins.button');
 
-print('LED GPIO:', led, 'button GPIO:', button);
-GPIO.set_mode(led, GPIO.MODE_OUTPUT);
 
+print('LED GPIO:', led, 'button GPIO:', button);
+
+// Blink built-in LED every second
+GPIO.set_mode(led, GPIO.MODE_OUTPUT);
+Timer.set(1000 /* 1 sec */, Timer.REPEAT, function() {
+  let value = GPIO.toggle(led);
+}, null);
 
 // Publish to MQTT topic on a button press. Button is wired to GPIO pin 0
 GPIO.set_button_handler(button, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 20, function() {
   print('button pressed')
   Azure.sendD2CMsg('btn=1', "t");
+  GPIO.toggle(led);
+  Sys.usleep(100000);
+  GPIO.toggle(led);
+  Sys.usleep(100000);
+  GPIO.toggle(led);
+  Sys.usleep(100000);
+  GPIO.toggle(led);
+  Sys.usleep(100000);
+  GPIO.toggle(led);
 }, null);
